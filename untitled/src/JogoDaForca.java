36430f806eb5f551de0c489536c5efe5329b4844
Juliano Machado <mach4d0@proton.me>
2025-11-27 13:45:29 -0300

    feat: adiciona lógica de tentativa e verificação de vitória/derrota

--- untitled/src/JogoDaForca.java
+++ untitled/src/JogoDaForca.java
@@ -25,4 +25,30 @@ public class JogoDaForca {
         JogoDaForca jogo = new JogoDaForca("java");
         jogo.exibirStatus();
     }
+    
+    public boolean tentarLetra(char letra) {
+        letra = Character.toLowerCase(letra);
+        boolean acertou = false;
+
+        for (int i = 0; i < palavraSecreta.length(); i++) {
+            if (palavraSecreta.charAt(i) == letra) {
+                palavraOculta.setCharAt(i * 2, letra);
+                acertou = true;
+            }
+        }
+
+        if (!acertou) {
+            tentativasRestantes--;
+        }
+
+        return acertou;
+    }
+
+    public boolean isJogoGanho() {
+        return !palavraOculta.toString().contains("_");
+    }
+
+    public boolean isJogoPerdido() {
+        return tentativasRestantes <= 0;
+    }
 }
