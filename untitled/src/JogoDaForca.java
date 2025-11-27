6aa70cf801efdb14ea71407c372626e2254221ad
Juliano Machado <mach4d0@proton.me>
2025-11-27 13:44:27 -0300

    feat: implementa estrutura básica do jogo da forca

--- untitled/src/JogoDaForca.java
+++ untitled/src/JogoDaForca.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class JogoDaForca {
+    private String palavraSecreta;
+    private StringBuilder palavraOculta;
+    private int tentativasRestantes;
 
-public class jugo {
+    public JogoDaForca(String palavra) {
+        this.palavraSecreta = palavra.toLowerCase();
+        this.tentativasRestantes = 6;
+        inicializarPalavraOculta();
+    }
+
+    private void inicializarPalavraOculta() {
+        palavraOculta = new StringBuilder();
+        for (int i = 0; i < palavraSecreta.length(); i++) {
+            palavraOculta.append("_ ");
+        }
+    }
+
+    public void exibirStatus() {
+        System.out.println("Palavra: " + palavraOculta);
+        System.out.println("Tentativas restantes: " + tentativasRestantes);
+    }
+
+    public static void main(String[] args) {
+        JogoDaForca jogo = new JogoDaForca("java");
+        jogo.exibirStatus();
+    }
 }
